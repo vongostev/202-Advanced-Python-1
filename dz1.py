deec7dc96b5fa2022580b4b895f6fff1d095f60b
Vorobev Vadim <78419088+Pauseferg@users.noreply.github.com>
2021-09-22 14:07:14 +0300

    Update dz1.py

--- dz1.py
+++ dz1.py
@@ -6,9 +6,8 @@ def is_even(a):
 
 def generate_squares(min_num, max_num):
     ans = []
-    for i in range(min_num, max_num+1):
-        if i**0.5 % 1 == 0:
-            ans.append(i)
+    for i in range(min_num, max_num + 1):
+        ans.append(i * i)
     return ans
 
 
@@ -32,15 +31,15 @@ def make_dict(l):
 
 
 class Vector2D:
-    def __init__(self, x, y):
+    def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
+    def norm(self):
+        return (self.x * self.x + self.y * self.y) ** (1 / 2)
+
     def __str__(self):
         return '(x, y) = ({}, {})'.format(self.x, self.y)
-
-    def norm(self):
-        return (self.x**2 + self.y**2)**0.5
 
     def __lt__(self, other):
         return self.norm() < other.norm()
@@ -49,34 +48,74 @@ class Vector2D:
         return self.norm() <= other.norm()
 
     def __eq__(self, other):
-        return self.norm() == other.norm()
+        return self.x == other.x and self.y == other.y
 
     def __ne__(self, other):
-        return self.norm() != other.norm()
+        return not self.__eq__(other)
 
     def __gt__(self, other):
-        return self.norm() > other.norm()
+        return not self <= other
 
     def __ge__(self, other):
-        return self.norm() >= other.norm()
+        return not self < other
 
     def __add__(self, other):
-        self.x += other.x
-        self.y += other.y
-        return self
+        return Vector2D(self.x + other.x, self.y + other.y)
 
     def __sub__(self, other):
-        self.x -= other.x
-        self.y -= other.y
-        return self
+        return Vector2D(self.x - other.x, self.y - other.y)
 
-    def __mul__(self, other):
-        self.x *= other
-        self.y *= other
-        return self
+    def __mul__(self, n):
+        return Vector2D(self.x * n, self.y * n)
+
+    def __rmul__(self, n):
+        return Vector2D(self.x * n, self.y * n)
 
 if __name__ == '__main__':
-    q = [-240, -17, 0, 12, 131]
-    a = [True, False, True, True, False]
-    for i in range(len(q)):
-        assert is_even(q[i] == a[i])
+
+    def test_is_even():
+        q = [-240, -17, 0, 12, 131]
+        a = [True, False, True, True, False]
+        for i in range(len(q)):
+            assert is_even(q[i] == a[i])
+        print('is_even - correct')
+
+    def test_generate_squares():
+        assert generate_squares(1, 0) == []
+        assert generate_squares(0, 2) == [0, 1, 4]
+        print('generate_squares - correct')
+
+    def test_split_list():
+        assert split_list([0, 0, 0]) == []
+        assert split_list([1, 2, 3]) == [1, 2, 3]
+        assert split_list([0, 0, 1]) == [1]
+        print('split_list - correct')
+
+    def test_make_dict():
+        assert make_dict([1, 2, 3]) == {'str': [], 'num': [1, 2, 3]}
+        assert make_dict([]) == {'str': [], 'num': []}
+        assert make_dict(['1', 1, '22', 22, '333', 333]) == {'str': [
+            ('1', 1), ('22', 2), ('333', 3)], 'num': [1, 22, 333]}
+        print('make_dict - correct')
+
+    def test_vector2D():
+        first = Vector2D(1, 1)
+        second = Vector2D(0, 1)
+        assert second.norm() == 1
+        assert (first == first)
+        assert (first == second) == False
+        assert (first > second)
+        assert (first < second) == False
+        assert (first <= second) == False
+        assert (first >= second)
+        assert 5 * first == Vector2D(5, 5)
+        assert second * 2 == Vector2D(0, 2)
+        assert (first + second) == Vector2D(1, 2)
+        assert (second - first) == Vector2D(-1, 0)
+        print('Vector2D - correct')
+
+    test_is_even()
+    test_generate_squares()
+    test_split_list()
+    test_make_dict()
+    test_vector2D()
