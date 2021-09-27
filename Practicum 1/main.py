528f9eb6a81e9453b7206f9676b9ec533b1b4992
Arseniy Uspenskiy <77883678+bziar@users.noreply.github.com>
2021-09-27 16:47:55 +0300

    Update main.py

--- Practicum 1/main.py
+++ Practicum 1/main.py
@@ -3,16 +3,18 @@ from collections import defaultdict
 with open('log.txt', 'r') as f:
     fun_count = 0
     my_dict = {}
+    mod_list = []
     for line in f:
         fun_count += line.count('fun:')
         if len(line) > 38:
             if line[37] == '[':
                 my_dict.setdefault(line[26:36].rstrip(), defaultdict(lambda: 0))
                 my_dict[line[26:36].rstrip()][line[38:45].rstrip()] += 1
+            if line[48:53] == 'Found':
+                if line[54:].split()[1] == 'modes':
+                    mod_list.append(line[54:].split()[0])
     for key in my_dict:
         my_dict[key] = dict(my_dict[key])
-    mod_count = len(my_dict)
-    mod_list = 'Found ' + str(mod_count) + ' modes'
     print(my_dict)
     print(f'The number of "fun": {fun_count}')
     print(mod_list)
