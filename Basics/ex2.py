e55b412bf9b13a1db6331e45241ce99eabff7be6
MagistrLoiso <dmtred.h@gmail.com>
2021-10-13 15:39:49 +0300

    Update ex2.py

--- Basics/ex2.py
+++ Basics/ex2.py
@@ -5,25 +5,30 @@ def read_file(file_name):
 
 
 def split_log_string(s):
-    s = s.split(' - ')
-    s += s.pop(1).split(' [', maxsplit=1)
-    s += s.pop(2).split(']  ', maxsplit=1)
-    s[2] = s[2].strip()
-    return s
+    try:
+        # Разбиение строки по шаблону
+        s = s.split(' - ')
+        s += s.pop(1).split(' [', maxsplit=1)
+        s += s.pop(2).split(']  ', maxsplit=1)
+        s[2] = s[2].strip()
+    except Exception:
+        # Если любая из функций разбиения возвращает ошибку,
+        # строка не удовлетворяет заданному шаблону, и её надо пропустить
+        return []
+    else:
+        return s
 
 
 def analyze_logs(file_name):
     output_dict = {}
     fun_count = 0
-    modes_dict = {'normal': 0, 'radial': 0}
+    modes_list = []
+    radial_modes_list = []
     message = []
     for s in read_file(file_name):
         fun_count += int(s.find('fun:') != -1)
-        try:
-            s = split_log_string(s)
-        except Exception:
-            pass
-        else:
+        s = split_log_string(s)
+        if s != []:
             if s[1] not in output_dict:
                 output_dict[s[1]] = {
                     'DEBUG': 0,
@@ -33,14 +38,15 @@ def analyze_logs(file_name):
             output_dict[s[1]][s[2]] += 1
             message = s[3].split()
             if message[0] == 'Found' and message[2] == 'modes':
-                modes_dict['normal'] += int(message[1])
+                modes_list.append(message[1])
             if message[0] == 'Found' and message[2] == 'radial' and message[3] == 'mode(s)':
-                modes_dict['radial'] += int(message[1])
-    return [output_dict, fun_count, modes_dict]
+                radial_modes_list.append(message[1])
+    return [output_dict, fun_count, modes_list, radial_modes_list]
 
 
 if __name__ == '__main__':
     result = analyze_logs('log.txt')
     print(result[0])
     print(f'fun_count: {result[1]}')
-    print(f'modes_count: {result[2]}')
+    print(f'modes_list: {result[2]}')
+    print(f'radial_modes_list: {result[3]}')
