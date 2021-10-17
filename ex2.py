ba19c91cb82afbf19664129e7c709acb579156bb
KimerJong <kitovnet911@icloud.com>
2021-10-17 16:09:00 +0300

    Update Numpy ex

--- ex2.py
+++ ex2.py
@@ -3,46 +3,45 @@ import matplotlib.pyplot as plt
 from scipy.special import factorial
 
 
-def Puasson(lambda1, N):
+def poisson(lambda1, N):
     if lambda1 < 0:
         raise IOError("Лямбда меньше 0")
     n = np.arange(0, N + 1)
-    p = (lambda1 ** n) * (np.exp(1) ** (-lambda1)) / factorial(n)
+    p = (lambda1 ** n) * np.exp(-lambda1) / factorial(n)
     return np.array([p, n])
 
 
-def Moment(arr, k):
+def moment(arr, k):
     if k < 0 or not isinstance(k, int) or not isinstance(arr, np.ndarray):
         raise IOError("Несоответствие данных на входе")
     return np.sum((arr[1] ** k) * arr[0])
 
 
-def Average(arr):
+def average(arr):
     if not isinstance(arr, np.ndarray):
         raise IOError("На входе НЕ массив numpy")
-    return Moment(arr, 1)
+    return moment(arr, 1)
 
 
-def Dispersion(arr):
+def dispersion(arr):
     if not isinstance(arr, np.ndarray):
         raise IOError("На входе НЕ массив numpy")
-    arr[1] = ((arr[1] - Average(arr)) ** 2)
-    return Average(arr)
+    return average(np.array([arr[0], ((arr[1] - average(arr)) ** 2)]))
 
 
 if __name__ == '__main__':
-    p1 = Puasson(5, 1000)
-    p2 = Puasson(7, 1000)
-    p3 = Puasson(10, 1000)
+    p1 = poisson(5, 1000)
+    p2 = poisson(7, 1000)
+    p3 = poisson(10, 1000)
 
-    assert np.allclose(Average(p1), 5, 1e-10)
-    assert np.allclose(Dispersion(p1), 5, 1e-10)
+    assert np.allclose(average(p1), 5, 1e-10)
+    assert np.allclose(dispersion(p1), 5, 1e-10)
 
-    assert np.allclose(Average(p2), 7, 1e-4)
-    assert np.allclose(Dispersion(p2), 7, 1e-4)
+    assert np.allclose(average(p2), 7, 1e-4)
+    assert np.allclose(dispersion(p2), 7, 1e-4)
 
-    assert np.allclose(Average(p3), 10, 1e-1)
-    assert np.allclose(Dispersion(p3), 10, 1e-1)
+    assert np.allclose(average(p3), 10, 1e-1)
+    assert np.allclose(dispersion(p3), 10, 1e-1)
 
     plt.plot(p1[0], label='lambda = 5')
     plt.plot(p2[0], label='lambda = 7')
