22677903f2b46fdd7e662f393480d3a6d4c2d8cb
Arseniy Uspenskiy <77883678+bziar@users.noreply.github.com>
2021-10-17 13:02:27 +0300

    Upenskiy202_last
    Исправил ту ошибку, она была связана с тем, что я неправильно подавал функции average данные среднеквадратичного отклонения. Теперь в допустимых пределах она работает (при больших лямбда ее все равно уносит, но тут уж неточность python).

--- Numpy/Uspenskiy202.py
+++ Numpy/Uspenskiy202.py
@@ -5,7 +5,7 @@ import scipy.special
 
 def poisson(лямбда, N):
     assert лямбда >= 0, 'Lambda should not be negative'
-    x = np.arange(0, N)
+    x = np.arange(N)
     y = лямбда ** x * np.exp(-лямбда) / scipy.special.factorial(x)
     return np.array([x, y])
 
@@ -21,16 +21,20 @@ def average(array):
 
 
 def dispersion(array):
-    return average((array[0] - average(array)) ** 2)
+    return average(np.array([(array[0] - average(array)) ** 2, array[1]]))
 
 
-лямбда = 1.755
-arr = poisson(лямбда, 17)
+лямбда = 3
+k = 2
+
+arr = poisson(лямбда, 150)
 
 plt.plot(arr[0], arr[1])
-print(moment(arr, 2))
-print(average(arr))
-print(dispersion(arr))
 plt.title("Poisson distribution")
 plt.show()
+
+print(f'Moment with k = {k} :', moment(arr, k))
+print('Average: ', average(arr))
+print('Dispersion: ', dispersion(arr))
+
 assert np.allclose([average(arr), dispersion(arr)], [лямбда, лямбда], 0.1)
