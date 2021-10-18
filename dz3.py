1a8aea0911169c259fdf68b5ccd879897b855360
Vorobev Vadim <78419088+Pauseferg@users.noreply.github.com>
2021-10-18 12:21:43 +0300

    Update dz3.py

--- dz3.py
+++ dz3.py
@@ -1,17 +1,18 @@
 import numpy as np
 
 
-def Poiss(l, N):
-    assert l > 0
-    return np.random.poisson(lam=l, size=N)
+def poiss(l, N):
+    assert l >= 0
+    x = np.arange(N)
+    y = l ** x * np.exp(-l) / scipy.special.factorial(x)
+    return np.array([x, y])
 
 
 def moment(k, n):
     assert k % 1 == 0
     assert type(n) == np.ndarray
-    prob = np.array(list(map(lambda x: (n == x).sum() / n.size, n)))
-    d = dict(zip(n, prob))
-    return (np.array(list(d.keys()))**k * np.array(list(d.values()))).sum()
+    return (np.unique(n ** k *
+                      np.array([(n == i).sum() / len(n) for i in n]))).sum()
 
 
 def mean(n):
@@ -25,13 +26,13 @@ def std(n):
 if __name__ == '__main__':
 
     def test_mean():
-        assert abs(mean(Poiss(5, 100000)) - 5) < 0.1
-        assert abs(mean(Poiss(10, 100000)) - 10) < 0.1
+        assert abs(mean(np.random.poisson(5, 100000)) - 5) < 0.1
+        assert abs(mean(np.random.poisson(10, 100000)) - 10) < 0.1
         print('mean_is_correct')
 
     def test_std():
-        assert abs(std(Poiss(5, 100000)) - 5) < 0.1
-        assert abs(std(Poiss(10, 100000)) - 10) < 0.1
+        assert abs(std(np.random.poisson(5, 100000)) - 5) < 0.1
+        assert abs(std(np.random.poisson(10, 100000)) - 10) < 0.1
         print('std_is_correct')
 
     test_mean()
