695d71f9b5374bfd055e3f6d6e66705eeef3f14c
Basssed <90472631+Basssed@users.noreply.github.com>
2021-10-28 17:49:05 +0300

    Create Sorokin_Stepan_ex2.py

---
+++ Sorokin_Stepan_ex2.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+with open('Log.txt', 'r') as f:
+    d = {}
+    count = 0
+    for line in f:
+        if len(line) > 38:
+            if line[37] == '[':
+                d.setdefault(line[26:36].rstrip(), defaultdict(lambda: 0))
+                d[line[26:36].rstrip()][line[38:45].rstrip()] += 1
+        count += line.count('fun:')
+    print('The number of "fun": {}'.format(count))
+    for i in d:
+        d[i] = dict(d[i])
+    print(d)
+    
