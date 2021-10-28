a2da7b85d56ee51b6cca9c73c8fcaafb211d609a
HauptMachine\vonGostev <gostev.pavel@physics.msu.ru>
2021-10-28 14:49:30 +0300

    Add Experimental data processing example

---
+++ Practicum/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 25 13:17:40 2021
+
+@author: Pavel Gostev
+"""
+import sys
+import os
+
+sys.path.append(os.path.abspath('../../lightprop2d'))
+sys.path.append(os.path.abspath('../../ghost_images'))
