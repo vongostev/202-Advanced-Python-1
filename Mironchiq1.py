3eb875bbb0c24a6ea1c65dac4a4de9f5e48eebba
programmmist228 <93081209+programmmist228@users.noreply.github.com>
2021-10-28 17:27:03 +0300

    Update Mironchiq1.py

--- Mironchiq1.py
+++ Mironchiq1.py
@@ -75,59 +75,66 @@ class Vector2D:
     def __rmul__(self, n):
         return Vector2D(self.x * n, self.y * n)
 
-    def test_is_even():
-        assert is_even(-240) == True
-        assert is_even(-17) == False
-        assert is_even(0) == True
-        assert is_even(12) == True
-        assert is_even(131) == False
 
-    def test_generate_squares():
-        assert generate_squares(1, 1) == [1]
-        assert generate_squares(-1, 1) == [1, 0, 1]
-        assert generate_squares(-2, -1) == [4, 1]
-        assert generate_squares(3, 6) == [9, 16, 25, 36]
-        assert generate_squares(0, 4) == [0, 1, 4, 9, 16]
+def test_is_even():
+    assert is_even(-240) == True
+    assert is_even(-17) == False
+    assert is_even(0) == True
+    assert is_even(12) == True
+    assert is_even(131) == False
 
-    def test_split_list():
-        assert split_list([0]) == []
-        assert split_list([0, 0, 0]) == []
-        assert split_list([-1, 2, 1]) == [-1, 2, 1]
-        assert split_list([-3, 0, 5]) == [-3, 5]
-        assert split_list([1, 0, 4, 0, 7]) == [1, 4, 7]
-        print('split_list correct')
 
-    def test_make_dict():
-        assert make_dict([1, 2, 3, 4, 5]) == {
-            'str': [], 'num': [1, 2, 3, 4, 5]}
-        assert make_dict([]) == {'str': [], 'num': []}
-        assert make_dict(['1', 1, '22', 22, '333', 333, '4444', 4444]) == {'str': [
-            ('1', 1), ('22', 2), ('333', 3), ('4444', 4)], 'num': [1, 22, 333, 4444]}
-        print('make_dict correct')
+def test_generate_squares():
+    assert generate_squares(1, 1) == [1]
+    assert generate_squares(-1, 1) == [1, 0, 1]
+    assert generate_squares(-2, -1) == [4, 1]
+    assert generate_squares(3, 6) == [9, 16, 25, 36]
+    assert generate_squares(0, 4) == [0, 1, 4, 9, 16]
 
-    def test_vector2D():
-        zero_vector = Vector2D()
-        first = Vector2D(1, 1)
-        second = Vector2D(-12, 5)
-        assert str(zero_vector) == '(0, 0)'
-        assert second.norm() == 13
-        assert (first == Vector2D(1, 1))
-        assert (first == second) == False
-        assert (first < second) == True
-        assert (first > Vector2D(-1, 0))
-        assert (first >= Vector2D(1, 1))
-        assert (first <= Vector2D(3, 4))
-        assert (first + second) == Vector2D(-11, 6)
-        assert (second - first) == Vector2D(-13, 4)
-        assert 5 * first == Vector2D(5, 5)
-        assert second * 2 == Vector2D(-24, 10)
-        print('Vector2D correct')
 
-    if __name__ == '__main__':
+def test_split_list():
+    assert split_list([0]) == []
+    assert split_list([0, 0, 0]) == []
+    assert split_list([-1, 2, 1]) == [-1, 2, 1]
+    assert split_list([-3, 0, 5]) == [-3, 5]
+    assert split_list([1, 0, 4, 0, 7]) == [1, 4, 7]
+    print('split_list correct')
 
-        test_is_even()
-        test_generate_squares()
-        test_split_list()
-        test_make_dict()
-        test_vector2D()
+
+def test_make_dict():
+    assert make_dict([1, 2, 3, 4, 5]) == {
+        'str': [], 'num': [1, 2, 3, 4, 5]}
+    assert make_dict([]) == {'str': [], 'num': []}
+    assert make_dict(['1', 1, '22', 22, '333', 333, '4444', 4444]) == {'str': [
+        ('1', 1), ('22', 2), ('333', 3), ('4444', 4)], 'num': [1, 22, 333, 4444]}
+    print('make_dict correct')
+
+
+def test_vector2D():
+    zero_vector = Vector2D()
+    first = Vector2D(1, 1)
+    second = Vector2D(-12, 5)
+    assert str(zero_vector) == '(0, 0)'
+    assert second.norm() == 13
+    assert (first == Vector2D(1, 1))
+    assert (first == second) == False
+    assert (first < second) == True
+    assert (first > Vector2D(-1, 0))
+    assert (first >= Vector2D(1, 1))
+    assert (first <= Vector2D(3, 4))
+    assert (first + second) == Vector2D(-11, 6)
+    assert (second - first) == Vector2D(-13, 4)
+    assert 5 * first == Vector2D(5, 5)
+    assert second * 2 == Vector2D(-24, 10)
+    print('Vector2D correct')
+
+
+if __name__ == '__main__':
+
+    test_is_even()
+    test_generate_squares()
+    test_split_list()
+    test_make_dict()
+    test_vector2D()
+
 
