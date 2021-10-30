5f4c5650a00abed99c6ae86aea18813abcaf4338
GV202 <91828889+GV202@users.noreply.github.com>
2021-10-30 11:15:48 +0300

    Add files via upload

---
+++ numpy_2.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 11:00:39 2021
+
+@author: ALEX
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import scipy.special as SCS
+
+def Poisson(Lambda, N):
+    assert Lambda >= 0
+    x = np.arange(0, N)
+    y = Lambda ** x * np.exp(-Lambda) / SCS.factorial(x)
+    return np.array([x, y])
+
+
+def Moment(my_array, k):
+    assert isinstance(k, int)
+    assert isinstance(my_array, np.ndarray)
+    return (my_array[0] ** k * my_array[1]).sum()
+
+
+
+def Average(my_array):
+    return Moment(my_array, 1)
+
+
+
+def Dispersion(my_array):
+    my_array[0] = (my_array[0] - Average(my_array)) ** 2
+    return Average(my_array)
+    
+
+
+if __name__ == '__main__':
+    Lambda = 4
+    my_array = Poisson(Lambda, 40)
+    print(Average(my_array))
+    print(Dispersion(my_array))
+    plt.plot(my_array[1])
+    plt.show()
