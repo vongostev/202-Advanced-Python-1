ab82795f605af54403ef15dc8f281161ffbc32ae
Egisess <92730200+Egisess@users.noreply.github.com>
2021-11-01 12:57:17 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -4,8 +4,36 @@ def read_file(filename):
             yield s
 
 
+def count_mode(s):
+    s = s.split('Found ')
+    if len(s) < 2:
+        return 0
+    s = s[1].split('modes')
+    if len(s) < 2:
+        return 0
+    try:
+        return int(s[0])
+    except(ValueError):
+        return 0
+
+
+def count_radial_mode(s):
+    s = s.split('Found ')
+    if len(s) < 2:
+        return 0
+    s = s[1].split('radial mode(s)')
+    if len(s) < 2:
+        return 0
+    try:
+        return int(s[0])
+    except(ValueError):
+        return 0
+
+
 def driller_file():
     fun_q = 0
+    mode_q = 0
+    r_mode_q = 0
     file_dic = {}
     for s in read_file('log.txt'):
         box = s.split(' - ')
@@ -22,6 +50,8 @@ def driller_file():
         message = '] '.join(type_message[1:])
         type_message = type_message[0].strip()
         fun_q += 'fun:' in message
+        mode_q += count_mode(message)
+        r_mode_q += count_radial_mode(message)
         if box not in file_dic:
             file_dic[box] = {
                 'DEBUG': 0,
@@ -29,10 +59,12 @@ def driller_file():
                 'WARNING': 0,
                 'ERROR': 0}
         file_dic[box][type_message] += 1
-    return (file_dic, fun_q)
+    return (file_dic, fun_q, mode_q, r_mode_q)
 
 
 if __name__ == '__main__':
     s = driller_file()
     print(s[0])
     print(f'fun_q: {s[1]}')
+    print(f'mode_q: {s[2]}')
+    print(f'r_mode_q: {s[3]}')
