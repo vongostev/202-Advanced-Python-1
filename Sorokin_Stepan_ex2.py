56cbedb733d02ed77fa367c5ab1ffabd3085a10c
Basssed <90472631+Basssed@users.noreply.github.com>
2021-11-04 22:08:20 +0300

    Update Sorokin_Stepan_ex2.py

--- Sorokin_Stepan_ex2.py
+++ Sorokin_Stepan_ex2.py
@@ -2,15 +2,19 @@ from collections import defaultdict
 
 with open('Log.txt', 'r') as f:
     d = {}
+    l = []
     count = 0
     for line in f:
         if len(line) > 38:
             if line[37] == '[':
                 d.setdefault(line[26:36].rstrip(), defaultdict(lambda: 0))
                 d[line[26:36].rstrip()][line[38:45].rstrip()] += 1
+            if (line[48:53] == 'Found') and (line[54:].split()[1] == 'modes'):
+                l.append(line[54:].split()[0])
         count += line.count('fun:')
     print('The number of "fun": {}'.format(count))
     for i in d:
         d[i] = dict(d[i])
-    print(d)
     
+    print(l)
+    print(d)
