66e2980696f62959ead00e439f277f4014d035bc
BeginTheorem <115108718+BeginTheorem@users.noreply.github.com>
2022-11-11 15:09:43 +0300

    Переименован. Отформатирован.

--- numpy_2/numpy_2.py
+++ numpy_2/numpy_2.py
@@ -3,34 +3,39 @@ import decimal
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 @np.vectorize
-def int2d (x):
+def int2d(x):
     y = float(x)
     return decimal.Decimal(y)
-#int --> Decimal
+# int --> Decimal
+
 
 @np.vectorize
-def power (x,l):
-    return decimal.Context().power(l,x)
-#Decimal x Decimal --> Decimal
+def power(x, l):
+    return decimal.Context().power(l, x)
+# Decimal x Decimal --> Decimal
+
 
 @np.vectorize
-def factorial (x):
+def factorial(x):
     if x == 0:
         return decimal.Decimal(1)
     else:
-        k = decimal.Decimal(1) 
-        for i in range(1,x+1,1):
+        k = decimal.Decimal(1)
+        for i in range(1, x+1, 1):
             k = k*decimal.Decimal(i)
         return k
-#int --> Decimal
+# int --> Decimal
 
-def poi (l,N):
+
+def poi(l, N):
     if l < 0:
         raise ValueError("l<0")
     X = np.arange(N+1)
-    return (power(int2d(X),decimal.Decimal(l))/factorial(X))*decimal.Context().exp(-decimal.Decimal(l))
-#float x int --> np.ndarray(decimal.Decimal)
+    return (power(int2d(X), decimal.Decimal(l))/factorial(X))*decimal.Context().exp(-decimal.Decimal(l))
+# float x int --> np.ndarray(decimal.Decimal)
+
 
 def moment(Y, a):
     if type(Y) != np.ndarray:
@@ -38,9 +43,10 @@ def moment(Y, a):
     if type(a) != int:
         raise TypeError("Порядок не целый")
     X = int2d(np.arange(Y.size))
-    X = power(decimal.Decimal(a),X)*Y
+    X = power(decimal.Decimal(a), X)*Y
     return np.sum(X)
-#np.ndarray(decimal.Decimal) x int --> decimal.Decimal
+# np.ndarray(decimal.Decimal) x int --> decimal.Decimal
+
 
 def average(Y):
     return moment(Y, 1)
@@ -58,7 +64,7 @@ def function_to_test(l, N, d):
         return True
     else:
         return False
-#float x int x float --> bool
+# float x int x float --> bool
 # Возвращает истину, если среднее и дисперсия распределения
 # отклоняются от правильных с относительной погрешностью менее d.
 
@@ -90,7 +96,7 @@ if __name__ == '__main__':
     assert function_to_test(3, 1000, 0.01) == True
     assert function_to_test(4, 100, 0.01) == True
     assert function_to_test(5, 100, 0.01) == True
-    assert function_to_test(5, 6, 0.01) == False # Мало точек
+    assert function_to_test(5, 6, 0.01) == False  # Мало точек
     assert function_to_test(6, 100, 0.01) == True  # Переполнения нет !!!
     assert function_to_test(500, 1000, 0.01) == True
     plt.plot(poi(500, 1000))
